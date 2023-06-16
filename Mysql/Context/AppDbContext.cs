7531bc5cdd9f6d523b7decfce418307521acd5c2
Marcus Vinicius(Vinny) <viniciuskawakami@hotmail.com>
2023-06-16 16:42:35 -0300

    Add project files.

---
+++ Mysql/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mysql.Models;
+
+namespace Mysql.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+        public DbSet<Cnae> Cnaes { get; set; }
+    }
+}
